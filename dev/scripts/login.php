295120d5e3bc835375d633dfc7dc6c736a129fa6
Phuoc Nguyen <ej77536@umbc.edu>
2017-11-18 01:50:18 -0500

    Add files via upload

--- dev/scripts/login.php
+++ dev/scripts/login.php
@@ -1,17 +1,18 @@
 <?php
 	require_once(dirname($_SERVER['DOCUMENT_ROOT']) . "/classes/Session.class.php");
 	require_once(dirname($_SERVER['DOCUMENT_ROOT']) . "/classes/User.class.php");
-	Session::start();
-	
+	Session::Start();
+
 	if(isset($_POST['submit']) && !empty($_POST)) 
 	{
 		$User_ID = $_POST['User_ID'];
 		$User_Password = $_POST['User_Password'];
-
+	
 		$_SESSION['CURRENT_USER'] = new User();
-		if ($_SESSION['CURRENT_USER']::Login($User_ID, $User_Password))
+		
+		if ($_SESSION['CURRENT_USER']->Login($User_ID, $User_Password))
 		{
-			header("Location: /Project/View.php");
+			header("Location: Project/View.php");
 		}
 		else
 		{
@@ -31,10 +32,10 @@
 			<div class="login_box">
 			
 				User ID: </br>
-				<input type="text" placeholder="Enter ID" name="User_ID"></br>
+				<input type="text" placeholder="Enter ID" name="User_ID" required></br>
 				
 				Password: </br>
-				<input type="password" placeholder="Enter Password" name="User_Password"></br>
+				<input type="password" placeholder="Enter Password" name="User_Password" required></br>
 				
 				</br>
 				<button type="submit" name="submit">Login</button>
