1d93e06787acd344d8929d92b89ba530da87c1d9
Cameron Hensel <kyameron343@gmail,com>
2017-11-20 17:04:39 -0500

    Add View and Style for projects
    Added base HTML and CSS for Project View

--- dev/scripts/Project/View.php
+++ dev/scripts/Project/View.php
@@ -1,2 +1,47 @@
 <?php
 ?>
+<html>
+	<head>
+		<meta charset=utf-8 />
+		<link href ="Style.css" rel="stylesheet">
+	</head>
+	<body>
+	
+
+		<div class="nav_bar">
+			
+			<!--List of Projects, Phases, and Tasks displays here-->
+			<ul id="project_list">
+				<li>Project 1</li>
+				<li>Project 2</li>
+				<li>Project 3</li>
+			</ul>
+		
+			<div class="nav_buttons">
+			
+				<button>Create Task</button>
+				<button>Create Phase</button>
+				
+			</div>
+		</div>
+		
+		<div class="display">
+			<h1>Project Name: <a id="Project_Name">Project 1</a></h1></br>
+			Project ID#: <a id="Project_ID">12345</a></p>
+			Next Milestone: <a id="Next_Milestone"></a></p>
+			Estimated Hours: <a id="Hours"></a></p>
+			Total Budget: <a id="Total_Budget"></a></br>
+			Remaining Budget: <a id="Remaining_Budget"></a><p>
+			Description: <a id="Description"></a></p>
+			
+			<button>Edit Project</button>
+		</div>
+		
+		
+		<script>
+			<!--Scripts for updating the display-->
+		</script>
+	</body>
+	<footer>
+	</footer>
+</html>
