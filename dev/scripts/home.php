5caaf5d7df64db4df429b87cda73af0572eba279
Cameron Hensel <kyameron343@gmail,com>
2017-11-20 21:39:41 -0500

    Add home.php
    Added the VERY basic html for the landing page after a user signs in

---
+++ dev/scripts/home.php
@@ -0,0 +1,52 @@
+<?php
+?>
+<html>
+	<head>
+		<meta charset=utf-8 />
+		<link href ="style.css" rel="stylesheet">
+	</head>
+	<body>
+	
+		<div class="title_bar">
+			<img src="">
+			<h1>Project Planer</h1>
+			<button>Sign Out</button>
+		</div>
+		<div class="project_menu">
+			<button>Create New Project</button>
+			<table>
+				<tr>
+					<th>Project Name</th>
+					<th>Next Milestone</th>
+					<th>Estimated Hours</th>
+					<th>Remaining Budget</th>
+					<th>View/Delete</th>
+				</tr>
+				<tr>
+					<td>Project 1</td>
+					<td>Milestone 2</td>
+					<td>50 hours</td>
+					<td>$5000</td>
+					<td>View/Delete</td>
+				</tr>
+				<tr>
+					<td>Project 2</td>
+					<td>Milestone 1</td>
+					<td>20 hours</td>
+					<td>$400</td>
+					<td>View/Delete</td>
+				</tr>
+				<tr>
+					<td>Project 3</td>
+					<td>Milestone 3</td>
+					<td>36 hours</td>
+					<td>$1</td>
+					<td>View/Delete</td>
+				</tr>
+			</table>
+		</div>	
+	
+	</body>
+	<footer>
+	</footer>
+</html>
